b5818fe978b14bbe8e5b536e519cdae79b84755b
Dobrslav <ivlevdobroslav@gmail.com>
2026-01-19 22:05:43 +0200

    0.1

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 
 client = socket.socket()
-hostname = socket.gethostname()
-port = 12345
+hostname = "192.168.0.107"
+port = 5000
 client.connect((hostname, port))
 message = input(": ")
 client.send(message.encode())
